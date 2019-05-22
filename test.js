f0509f649ce81f5ecb6ff4ac8e959720be86e3bb
Max Kharandziuk <max.kharandziuk@gmail.com>
2019-05-22 13:03:34 +0200

    add parse function and test for it

--- test.js
+++ test.js
@@ -1,7 +1,20 @@
 const { assert } = require('chai')
+const moment = require('moment')
 
 const solution = (input) => {
   return { productionCycle: 2 }
+}
+
+const parse = (input) => {
+  const firstDate = moment.min(
+      (input.map(el => moment(el.startingDay)))
+  )
+
+  return input.map(({startingDay, duration}) => {
+    var offsetInDays = moment.duration(moment(startingDay).diff(firstDate))
+        .asDays()
+    return [offsetInDays, offsetInDays + duration - 1]
+  })
 }
 
 it('can solve the first sample input', () =>{
@@ -50,4 +63,21 @@ it('can solve the second sample input', () =>{
       }
   ])
   assert.deepEqual(result, { productionCycle: 4 })
+})
+
+it('can parser the input', () =>{
+  const result = parse([
+      {
+        "startingDay": "2018-01-01T00:00:00.000Z",
+        "duration": 5
+      },
+      {
+        "startingDay": "2018-01-05T00:00:00.000Z",
+        "duration": 3
+      },
+  ])
+  assert.deepEqual(result, [
+      [0, 4],
+      [4, 6]
+  ])
 })
